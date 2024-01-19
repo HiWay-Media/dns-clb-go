7a09f4a6952c5b9c755b4ccce160ffe3d3568abf
Allan Nava <allannava95@gmail.com>
2024-01-19 15:29:04 +0100

    added NewDefaultTtlClb

--- clb/client.go
+++ clb/client.go
@@ -50,6 +50,12 @@ func NewTtlCacheClb(address string, port string, lbType LoadBalancerType, ttl in
 	return buildClb(cache, lbType)
 }
 
+func NewDefaultTtlClb(  ttl int ) LoadBalancer {
+	lib := dns.NewDefaultLookupLib()
+	cache := ttlcache.NewTtlCache(lib, ttl)
+	return buildClb(cache, lbType)
+}
+
 func buildClb(lib dns.Lookup, lbType LoadBalancerType) LoadBalancer {
 	switch lbType {
 	case RoundRobin:
